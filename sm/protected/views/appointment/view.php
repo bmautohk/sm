bfd8c9f902ce6a067ab56436b58e5f9929633c6f
bmautohk <rickykei@bmautohk.com>
2013-09-25 23:00:31 +0800

    Update view.php
    Add image upload

--- sm/protected/views/appointment/view.php
+++ sm/protected/views/appointment/view.php
@@ -19,8 +19,26 @@ $this->menu=array(
 		//'authorize_username',
 		'appointment_date',
 		'subject',
+		'subtopic',
 		'content',
 		'remark',
+		'followup_date',
+		array(
+			'type'=>'raw',
+			'value'=>$model->img1?html_entity_decode(CHtml::image(BuildUrl::imageUrl(GlobalConstants::DIR_APPOINTMENT_IMG.'/'.$model->img1),'alt',array('width'=>'150'))):null,
+		),
+		array(
+			'type'=>'raw',
+			'value'=>$model->img2?html_entity_decode(CHtml::image(BuildUrl::imageUrl(GlobalConstants::DIR_APPOINTMENT_IMG.'/'.$model->img2),'alt',array('width'=>'150'))):null,
+		),
+		array(
+			'type'=>'raw',
+			'value'=>$model->img3?html_entity_decode(CHtml::image(BuildUrl::imageUrl(GlobalConstants::DIR_APPOINTMENT_IMG.'/'.$model->img3),'alt',array('width'=>'150'))):null,
+		),
+		array(
+			'type'=>'raw',
+			'value'=>$model->img4?html_entity_decode(CHtml::image(BuildUrl::imageUrl(GlobalConstants::DIR_APPOINTMENT_IMG.'/'.$model->img4),'alt',array('width'=>'150'))):null,
+		),
 		'creation_date',
 		'modify_date',
 	),
